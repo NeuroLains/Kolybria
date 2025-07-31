2baaba2e20c8630d43d9379cd0c539039ca4d82e
NeuroLains <120753805+NeuroLains@users.noreply.github.com>
2025-07-31 14:58:29 +0300

    git add

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/Kolybria/', // 🚨 Критически важно для GitHub Pages! 
-  plugins: [vue()],
   plugins: [react()],
   server: {
     port: 3000
